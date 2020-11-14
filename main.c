a345908adc8145fc3e8fbaa004c5c5014f91a60f
Zukafudo <46060998+0xOutis@users.noreply.github.com>
2020-11-15 01:19:03 +0700

    Update main.c

--- main.c
+++ main.c
@@ -12,51 +12,51 @@
 
 int main(int argc, char **argv) {
 
-	if (argc < 0x3) {
-		LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[options]\n", argv[0x0]);
+	if (argc < 3) {
+		LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[options]\n", argv[0]);
 		help();
 		return 0x1;
 	}
 
 	ipaddr_t *addr = (ipaddr_t*)malloc(sizeof(ipaddr_t));
-	moveBuffer(argv[0x1], addr);
+	moveBuffer(argv[1], addr);
 	
-	if ((strcmp(argv[0x2], LONG_ARG_C) == 0x0) || (strcmp(argv[0x2], SHORT_ARG_C) == 0x0)){
-		if (addr->prefix <= 0x1e && addr->prefix >= 0x18)
+	if ((strcmp(argv[2], LONG_ARG_C) == 0) || (strcmp(argv[2], SHORT_ARG_C) == 0)){
+		if (addr->prefix <= 30 && addr->prefix >= 24)
 			prefix24_30(addr);
 		else {
-			LOG_ERROR("Error, Examples: %s [ipaddr/prefix] -[options]\n", argv[0x0]);
+			LOG_ERROR("Error, Examples: %s [ipaddr/prefix] -[options]\n", argv[0]);
 			help();
 			free(addr);
 			return 0x1;
 		}
-	} else if ((strcmp(argv[0x2], LONG_ARG_B) == 0x0) || (strcmp(argv[0x2], SHORT_ARG_B) == 0x0)){
-		if (addr->prefix <= 0x1e && addr->prefix >= 0x18)
+	} else if ((strcmp(argv[2], LONG_ARG_B) == 0) || (strcmp(argv[2], SHORT_ARG_B) == 0)){
+		if (addr->prefix <= 30 && addr->prefix >= 24)
 			prefix24_30(addr);
-		else if (addr->prefix <= 0x17 && addr->prefix >= 0x10)
+		else if (addr->prefix <= 23 && addr->prefix >= 16)
 			prefix16_23(addr);
 		else {
-			LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[option]\n", argv[0x0]);
+			LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[option]\n", argv[0]);
 			help();
 			free(addr);
 			return 0x1;
 		}
 		
-	} else if ((strcmp(argv[0x2], LONG_ARG_A) == 0x0) || (strcmp(argv[0x2], SHORT_ARG_A) == 0x0)){
-		if (addr->prefix <= 0x1e && addr->prefix >= 0x18)
+	} else if ((strcmp(argv[2], LONG_ARG_A) == 0) || (strcmp(argv[2], SHORT_ARG_A) == 0)){
+		if (addr->prefix <= 30 && addr->prefix >= 24)
 			prefix24_30(addr);
-		else if (addr->prefix <= 0x17 && addr->prefix >= 0x10)
+		else if (addr->prefix <= 23 && addr->prefix >= 16)
 			prefix16_23(addr);
-		else if (addr->prefix <= 0x0f && addr->prefix >=0x08)
+		else if (addr->prefix <= 15 && addr->prefix >= 8)
 			prefix8_15(addr);
 		else {
-			LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[option]\n", argv[0x0]);
+			LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[option]\n", argv[0]);
 			help();
 			free(addr);
 			return 0x1;
 		}
 	} else {
-		LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[option]\n", argv[0x0]);
+		LOG_ERROR("Error, Example: %s [ipaddr/prefix] -[option]\n", argv[0]);
 		help();
 	}
 
