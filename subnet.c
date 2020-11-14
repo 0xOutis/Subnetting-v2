93f67931676aa8dd535a77eedcb895cc316aa8c4
Zukafudo <46060998+0xOutis@users.noreply.github.com>
2020-11-15 01:14:41 +0700

    Update subnet.c

--- subnet.c
+++ subnet.c
@@ -5,8 +5,8 @@
 #include "subnet.h"
 
 int host;
-int network=0x0,broadcast=0x0;
-int numbers=0x1;
+int network=0,broadcast=0;
+int numbers=1;
 
 void help() {
 	puts("Options: ");
@@ -23,20 +23,20 @@ void help() {
 }
 
 void moveBuffer(void *buffer, ipaddr_t *ip) {
-	char *address = (char*)malloc(0x20*sizeof(char));
+	char *address = (char*)malloc(32*sizeof(char));
         strcpy(address, buffer);
-        char tmp[0x3];
-        int y=0x0, x=0x0;
+        char tmp[3];
+        int y=0, x=0;
 
-        for (int i=0x0;i<strlen(address);i++) {
+        for (int i=0;i<strlen(address);i++) {
                 if ((address[i] != __DOT_) && (address[i] != __PREFIX_)) {                                                                                          tmp[x] = address[i];                                                  x++;
                 }
                 else if ((address[i] == __DOT_) || (address[i] == __PREFIX_)) {                                                                                     ip->octet[y] = atoi(tmp);
                         memset(tmp,0x00,sizeof(tmp));
-                        x=0x0;
+                        x=0;
                         y++;
                         if (address[i] == __PREFIX_) {
-                                for (int j=(i+0x1);j<=strlen(address);j++) {
+                                for (int j=(i+1);j<=strlen(address);j++) {
                                         tmp[x] = address[j];
                                         x++;
                                 }
@@ -50,22 +50,22 @@ void moveBuffer(void *buffer, ipaddr_t *ip) {
 
 void prefix24_30(ipaddr_t *ip) {
 
-	ip->object.grade[0x0] = 0x20 - ip->prefix;
-	ip->object.subnet[0x0] = pow (2, ip->object.grade[0x0]);
-	ip->object.mask = 0x100 - ip->object.subnet[0x0];
+	ip->object.grade[0] = 32 - ip->prefix;
+	ip->object.subnet[0] = pow (2, ip->object.grade[0]);
+	ip->object.mask = 256 - ip->object.subnet[0];
 
-	printf("IP Address: %d.%d.%d.%d\n", ip->octet[0x0], ip->octet[0x1], ip->octet[0x2], ip->octet[0x3]);
+	printf("IP Address: %d.%d.%d.%d\n", ip->octet[0], ip->octet[1], ip->octet[2], ip->octet[3]);
 	printf("Subnetmask: %d.%d.%d.%d\n", 0xff, 0xff, 0xff, ip->object.mask);
-	host = ip->object.subnet[0x0] - 0x2;
+	host = ip->object.subnet[0] - 2;
 	printf("Host available: %d\n", host);
 
 	LINE
 	printf(" Network\t\t Broadcast\n");
 	do {
-		printf("%d.%d.%d."BOLDRED"%d\t"WHT, ip->octet[0x0], ip->octet[0x1], ip->octet[0x2], network);
-		network += ip->object.subnet[0x0];
-		broadcast = network - 0x1;
-		printf("  -\t%d.%d.%d."BOLDRED"%d\t"WHT"->[%d]\n", ip->octet[0x0], ip->octet[0x1], ip->octet[0x2], broadcast, numbers);
+		printf("%d.%d.%d."BOLDRED"%d\t"WHT, ip->octet[0], ip->octet[1], ip->octet[2], network);
+		network += ip->object.subnet[0];
+		broadcast = network - 1;
+		printf("  -\t%d.%d.%d."BOLDRED"%d\t"WHT"->[%d]\n", ip->octet[0], ip->octet[1], ip->octet[2], broadcast, numbers);
 		numbers++;
 	} while(network <= 0xff);
 	LINE
@@ -73,24 +73,24 @@ void prefix24_30(ipaddr_t *ip) {
 
 void prefix16_23(ipaddr_t *ip) {
 
-	ip->object.grade[0x0] = 0x20 - ip->prefix;
-	ip->object.grade[0x1] = 0x18 - ip->prefix;
-	ip->object.subnet[0x0] = pow (2, ip->object.grade[0x0]);
-	ip->object.subnet[0x1] = pow (2, ip->object.grade[0x1]);
-	ip->object.mask = 0x100 - ip->object.subnet[0x1];
+	ip->object.grade[0] = 32 - ip->prefix;
+	ip->object.grade[1] = 24 - ip->prefix;
+	ip->object.subnet[0] = pow (2, ip->object.grade[0]);
+	ip->object.subnet[1] = pow (2, ip->object.grade[1]);
+	ip->object.mask = 256 - ip->object.subnet[1];
 
-	printf("IP Address: %d.%d.%d.%d\n", ip->octet[0x0], ip->octet[0x1], ip->octet[0x2], ip->octet[0x3]);
-	printf("Subnetmask: %d.%d.%d.%d\n", 0xff, 0xff, ip->object.mask, 0x0);
-	host = ip->object.subnet[0x0] - 0x2;
+	printf("IP Address: %d.%d.%d.%d\n", ip->octet[0], ip->octet[1], ip->octet[2], ip->octet[3]);
+	printf("Subnetmask: %d.%d.%d.%d\n", 0xff, 0xff, ip->object.mask, 0x00);
+	host = ip->object.subnet[0] - 2;
 	printf("Host available: %d\n", host);
 
 	LINE
 	printf(" Network\t\t Broadcast\n");
 	do {
-		printf("%d.%d."BOLDRED"%d.%d\t"WHT, ip->octet[0x0], ip->octet[0x1], network, 0x0);
-		network += ip->object.subnet[0x1];
-		broadcast = network - 0x1;
-		printf("  -\t%d.%d."BOLDRED"%d.%d\t"WHT"->[%d]\n", ip->octet[0x0], ip->octet[0x1], broadcast, 0xff, numbers);
+		printf("%d.%d."BOLDRED"%d.%d\t"WHT, ip->octet[0], ip->octet[1], network, 0x00);
+		network += ip->object.subnet[1];
+		broadcast = network - 1;
+		printf("  -\t%d.%d."BOLDRED"%d.%d\t"WHT"->[%d]\n", ip->octet[0], ip->octet[1], broadcast, 0xff, numbers);
 		numbers++;
 	} while(network <= 0xff);
 	LINE
@@ -98,24 +98,24 @@ void prefix16_23(ipaddr_t *ip) {
 
 void prefix8_15(ipaddr_t *ip) {
 
-	ip->object.grade[0x0] = 0x20 - ip->prefix;
-	ip->object.grade[0x1] = 0x10 - ip->prefix;
-	ip->object.subnet[0x0] = pow (2, ip->object.grade[0x0]);
-	ip->object.subnet[0x1] = pow (2, ip->object.grade[0x1]);
-	ip->object.mask = 0x100 - ip->object.subnet[0x01];
+	ip->object.grade[0] = 32 - ip->prefix;
+	ip->object.grade[1] = 16 - ip->prefix;
+	ip->object.subnet[0] = pow (2, ip->object.grade[0]);
+	ip->object.subnet[1] = pow (2, ip->object.grade[1]);
+	ip->object.mask = 256 - ip->object.subnet[1];
 
-	printf("IP Address: %d.%d.%d.%d\n", ip->octet[0x0], ip->octet[0x1], ip->octet[0x2], ip->octet[0x3]);
-	printf("Subnetmask: %d.%d.%d.%d\n", 0xff, ip->object.mask, 0x0, 0x0);
-	host = ip->object.subnet[0x0] - 0x2;
+	printf("IP Address: %d.%d.%d.%d\n", ip->octet[0], ip->octet[1], ip->octet[2], ip->octet[3]);
+	printf("Subnetmask: %d.%d.%d.%d\n", 0xff, ip->object.mask, 0x00, 0x00);
+	host = ip->object.subnet[0] - 2;
 	printf("Host available: %d\n", host);
 
 	LINE
 	printf(" Network\t\t Broadcast\n");
 	do {
-		printf("%d."BOLDRED"%d.%d.%d\t"WHT, ip->octet[0x0], network, 0x0, 0x0);
+		printf("%d."BOLDRED"%d.%d.%d\t"WHT, ip->octet[0], network, 0x00, 0x00);
 		network += ip->object.subnet[0x1];
-		broadcast = network - 0x1;
-		printf(" -\t%d."BOLDRED"%d.%d.%d\t"WHT"->[%d]\n", ip->octet[0x0], broadcast, 0xff, 0xff, numbers);
+		broadcast = network - 1;
+		printf(" -\t%d."BOLDRED"%d.%d.%d\t"WHT"->[%d]\n", ip->octet[0], broadcast, 0xff, 0xff, numbers);
 		numbers++;
 	} while(network <= 0xff);
 	LINE
