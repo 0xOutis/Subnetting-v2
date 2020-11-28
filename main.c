9a6b1338113dd72af6bed5664259cbc1b8958779
Zukafudo <46060998+0xOutis@users.noreply.github.com>
2020-11-28 22:40:27 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -4,10 +4,11 @@
  * Tool    : Subnetting V2
 */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "subnet.h"
-
+#include "subnet.h" /* The header file contains */
+		    /* the subnetting function declaration */
 
 int main(int argc, char **argv) {
 
@@ -18,10 +19,12 @@ int main(int argc, char **argv) {
 	}
 
 	ipaddr_t *addr = (ipaddr_t*)malloc(sizeof(ipaddr_t));
-	moveBuffer(argv[1], addr);
+	moveBuffer(argv[1], addr); /* Move argv[1] to struct ipaddr_t*/
 	
+	/* IP class C */
 	if ((strcmp(argv[2], LONG_ARG_C) == 0) || (strcmp(argv[2], SHORT_ARG_C) == 0)){
-		if (addr->prefix <= 30 && addr->prefix >= 24)
+		/* for Prefix range 24-30 */
+		if (addr->prefix <= 0x1e && addr->prefix >= 0x18)
 			RANGE_CLASS_C_24(addr);
 		else {
 			LOG_ERROR("Error, Examples: %s [ipaddr/prefix] [options]\n", argv[0]);
@@ -29,10 +32,13 @@ int main(int argc, char **argv) {
 			free(addr);
 			return 0x1;
 		}
+	/* IP class B */
 	} else if ((strcmp(argv[2], LONG_ARG_B) == 0) || (strcmp(argv[2], SHORT_ARG_B) == 0)){
-		if (addr->prefix <= 30 && addr->prefix >= 24)
+		/* for Prefix range 24-30 */
+		if (addr->prefix <= 0x1e && addr->prefix >= 0x18)
 			RANGE_CLASS_B_24(addr);
-		else if (addr->prefix <= 23 && addr->prefix >= 16)
+	        /* for Prefix range 16-23 */	
+		else if (addr->prefix <= 0x17 && addr->prefix >= 0x10)
 			RANGE_CLASS_B_16(addr);
 		else {
 			LOG_ERROR("Error, Example: %s [ipaddr/prefix] [option]\n", argv[0]);
@@ -40,13 +46,16 @@ int main(int argc, char **argv) {
 			free(addr);
 			return 0x1;
 		}
-		
+	/* IP class A */	
 	} else if ((strcmp(argv[2], LONG_ARG_A) == 0) || (strcmp(argv[2], SHORT_ARG_A) == 0)){
-		if (addr->prefix <= 30 && addr->prefix >= 24)
+		/* for Prefix range 24-30 */
+		if (addr->prefix <= 0x1e && addr->prefix >= 0x18)
 			RANGE_CLASS_A_24(addr);
-		else if (addr->prefix <= 23 && addr->prefix >= 16)
+		/* for Prefix range 16-23 */
+		else if (addr->prefix <= 0x17 && addr->prefix >= 0x10)
 			RANGE_CLASS_A_16(addr);
-		else if (addr->prefix <= 15 && addr->prefix >= 8)
+		/* for Prefix range 8-15 */
+		else if (addr->prefix <= 0x0f && addr->prefix >= 0x08)
 			RANGE_CLASS_A_8(addr);
 		else {
 			LOG_ERROR("Error, Example: %s [ipaddr/prefix] [option]\n", argv[0]);
