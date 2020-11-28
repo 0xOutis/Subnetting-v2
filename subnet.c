760a3eacc1a8f47fd119fb015170e1c535a06fbc
Zukafudo <46060998+0xOutis@users.noreply.github.com>
2020-11-28 17:14:28 +0700

    Update subnet.c

--- subnet.c
+++ subnet.c
@@ -25,7 +25,7 @@ void help() {
 void moveBuffer(void *buffer, ipaddr_t *ip) {
 	char *address = (char*)malloc(32*sizeof(char));
         strcpy(address, buffer);
-        char tmp[3];
+        char tmp[4];
         short y=0, x=0;
 
         for (int i=0;i<strlen(address);i++) {
